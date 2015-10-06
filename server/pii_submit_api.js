994a0cb5d702f8042bca55a99ee5b35dc7aee719
Jackson Broussard <jacksonbrsrd@gmail.com>
2015-10-06 15:53:27 -0700

    Added option to test

--- server/pii_submit_api.js
+++ server/pii_submit_api.js
@@ -4,7 +4,8 @@ HTTP.methods({
 		var result = Meteor.http.get(url, {timeout: 300000});
 		if(result.statusCode==200) {
 			var generated_data = Meteor.call("generate_xml_from_json", this.params.journal_name, JSON.parse(result.content));
-			var submission_result = Meteor.call("submit_JSON_string_as_XML", generated_data.json_string);
+			var test = (this.query != void 0);
+			var submission_result = Meteor.call("submit_JSON_string_as_XML", generated_data.json_string, test);
 			return submission_result;
 		} else {
 			throw new Meteor.Error(result.statusCode, result.content);
