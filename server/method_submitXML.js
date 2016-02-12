b8c25e19644939e2111917437458f91af877e4a2
spasiu <stasi@smooch.io>
2016-02-12 08:06:50 -0500

    refactor frontend

--- server/method_submitXML.js
+++ server/method_submitXML.js
@@ -1,23 +1,19 @@
 Meteor.methods({
-	submit_JSON_string_as_XML: function(json_string, test) {
-		var js2xmlparser = Meteor.npmRequire("js2xmlparser");
+  submit_xml: function(xml, test) {
+    var options = {
+      headers: {
+        "Authorization": "Basic " + Base64.encode(Meteor.settings.cr_cred_un + ":" + Meteor.settings.cr_cred_pw),
+        "Content-Type": "application/vnd.crossref.deposit+xml"
+      },
+      content: xml
+    };
 
-		var xml_from_json = js2xmlparser("doi_batch", JSON.parse(json_string));
+    var encoded_pingback = encodeURIComponent("doi.oncotarget.com/deposit_pingback");
+    var response = Meteor.http.call("POST", "https://api.crossref.org/deposits?" + (test ? "test=true&" : "") + "pingback=" + encoded_pingback, options);
 
-		var options = {
-			headers: {
-				"Authorization": "Basic " + Base64.encode(Meteor.settings.cr_cred_un + ":" + Meteor.settings.cr_cred_pw),
-				"Content-Type": "application/vnd.crossref.deposit+xml"
-			},
-			content: xml_from_json
-		};
-
-		var encoded_pingback = encodeURIComponent("doi.oncotarget.com/deposit_pingback");
-		var response = Meteor.http.call("POST", "https://api.crossref.org/deposits?"+(test?"test=true&":"")+"pingback="+encoded_pingback, options);
-
-		return {
-			code: response.statusCode,
-			location: (response.headers.location || "")
-		};
-	}
+    return {
+      code: response.statusCode,
+      location: (response.headers.location || "")
+    };
+  }
 })
