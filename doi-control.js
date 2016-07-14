5bd60e18972817572e51cd6279480e0773378553
Jessica Lawshe <jessica@jlawshe.com>
2016-07-14 14:59:41 -0500

    Indentions and semicolons

--- doi-control.js
+++ doi-control.js
@@ -1,152 +1,152 @@
 if (Meteor.isClient) {
-  Session.set('registeredPiis', [])
+    Session.set('registeredPiis', []);
+    Session.set('isVolume', true);
+    Session.set('xml', '');
 
-  Session.set('isVolume', true)
-  Session.set('xml', '')
-
-  Template.form.helpers({
-    isVolume: function() {
-      return Session.get('isVolume')
-    }
-  })
-
-  Template.form.helpers({
-    registeredPiis: function() {
-      return Session.get('registeredPiis')
-    }
-  })
-
-  Template.xml.events({
-    'click .submit-xml': function(e) {
-      var xml = Session.get('xml')
-      if (!Meteor.utils.isValidXml(xml)) {
-        return feedback('Invalid XML')
-      }
-      e.target.innerText = 'Submitting...'
-      Meteor.call('submit_xml', xml, function(err, data) {
-        e.target.innerText = 'Submit XML'
-        var error = err && err.error || data.error
-        if (err) {
-          return feedback(err.error)
+    Template.form.helpers({
+        isVolume: function() {
+            return Session.get('isVolume');
         }
-        feedback('XML Submitted', true)
-        clearEditor()
-      })
-    }
-  })
+    });
 
-  Template.form.events({
-    'change .journal': function(e) {
-      Session.set('journal', e.target.value)
-      Session.set('registeredPiis', [])
-    },
-    'blur .piilist': function(e) {
-      var registeredPiis = Session.get('registeredPiis')
-      var blockRequests = Session.get('blockRequests')
-      var inputs = e.target.value
-        .split(',')
-        .map(function(pii) {
-          return pii.trim()
-        })
-
-      updateRegistered(inputs, registeredPiis)
-
-      inputs
-        .filter(function(pii) {
-          // check that pii has not been loaded
-          return registeredPiis.map(function(item) {
-            return item.pii
-          }).indexOf(pii) === -1
-        })
-        .forEach(function(pii) {
-          Meteor.call('checkRegistration', Session.get('journal'), pii, function(err, body) {
-            registeredPiis.push({
-              pii: pii,
-              status: !!(!err && body)
-            })
-            updateRegistered(inputs, registeredPiis)
-          })
-        })
-
-    },
-    'click .switch': function(e) {
-      var isVolumeInverted = Session.get('isVolume') ? false : true
-      Session.set('isVolume', isVolumeInverted)
-      e.target.innerText = isVolumeInverted ?
-        'Switch to PII submission' :
-        'Switch to volume submission'
-    },
-    'submit .generate-xml-form': function(e) {
-      var button = e.target.querySelector('button')
-      var journal = e.target.journal.value,
-        volume = e.target.volume && e.target.volume.value,
-        issue = e.target.issue && e.target.issue.value,
-        piilist = e.target.piilist && e.target.piilist.value,
-        date = e.target.date.value
-      e.preventDefault()
-      button.innerText = 'Generating...'
-      piilist ?
-        Meteor.call('get_json_and_generate_xml_from_pii_list', journal, piilist, date, cb) :
-        Meteor.call('get_json_and_generate_xml', journal, volume, issue, date, cb)
-
-      function cb(err, data) {
-        button.innerText = 'Generate submission'
-        if (err) {
-          feedback(err.error)
+    Template.form.helpers({
+        registeredPiis: function() {
+            return Session.get('registeredPiis');
         }
-        loadEditor(data.xml)
-        e.target.reset()
-      }
+    });
+
+    Template.xml.events({
+        'click .submit-xml': function(e) {
+            var xml = Session.get('xml');
+            if (!Meteor.utils.isValidXml(xml)) {
+                return feedback('Invalid XML');
+            }
+
+            e.target.innerText = 'Submitting...';
+
+            Meteor.call('submit_xml', xml, function(err, data) {
+                e.target.innerText = 'Submit XML';
+                var error = err && err.error || data.error;
+                if (err) {
+                    return feedback(err.error);
+                }
+                feedback('XML Submitted', true);
+                clearEditor();
+            });
+        }
+    });
+
+    Template.form.events({
+        'change .journal': function(e) {
+            Session.set('journal', e.target.value);
+            Session.set('registeredPiis', []);
+        },
+        'blur .piilist': function(e) {
+            var registeredPiis = Session.get('registeredPiis');
+            var blockRequests = Session.get('blockRequests');
+            var inputs = e.target.value
+                .split(',')
+                .map(function(pii) {
+                    return pii.trim();
+                });
+
+                updateRegistered(inputs, registeredPiis);
+
+                inputs
+                    .filter(function(pii) {
+                        // check that pii has not been loaded
+                        return registeredPiis.map(function(item) {
+                            return item.pii;
+                        }).indexOf(pii) === -1;
+                    })
+                    .forEach(function(pii) {
+                        Meteor.call('checkRegistration', Session.get('journal'), pii, function(err, body) {
+                            registeredPiis.push({
+                                pii: pii,
+                                status: !!(!err && body)
+                            });
+                            updateRegistered(inputs, registeredPiis);
+                        });
+                    });
+        },
+        'click .switch': function(e) {
+            var isVolumeInverted = Session.get('isVolume') ? false : true;
+            Session.set('isVolume', isVolumeInverted);
+            e.target.innerText = isVolumeInverted ?
+                'Switch to PII submission' :
+                'Switch to volume submission';
+        },
+        'submit .generate-xml-form': function(e) {
+            var button = e.target.querySelector('button');
+            var journal = e.target.journal.value,
+            volume = e.target.volume && e.target.volume.value,
+            issue = e.target.issue && e.target.issue.value,
+            piilist = e.target.piilist && e.target.piilist.value,
+            date = e.target.date.value;
+            e.preventDefault();
+            button.innerText = 'Generating...';
+            piilist ?
+                Meteor.call('get_json_and_generate_xml_from_pii_list', journal, piilist, date, cb) :
+                Meteor.call('get_json_and_generate_xml', journal, volume, issue, date, cb);
+
+            function cb(err, data) {
+                button.innerText = 'Generate submission';
+                if (err) {
+                    feedback(err.error);
+                }
+                loadEditor(data.xml);
+                e.target.reset();
+            }
+        }
+    });
+
+    Router.route('/', function() {
+        this.render('home');
+        AceEditor.instance('xmlbox', {
+            theme: 'dawn',
+            mode: 'xml'
+        }, function(editor) {
+            editor.setReadOnly(true);
+            editor.setOptions({
+                maxLines: Infinity,
+                showLineNumbers: false
+            });
+            editor.on('change', function() {
+                Session.set('xml', editor.getValue());
+            });
+        });
+    });
+
+    function updateRegistered(inputs, registeredPiis) {
+        Session.set('registeredPiis', registeredPiis.filter(function(registered) {
+            return inputs.indexOf(registered.pii) !== -1;
+        }));
     }
-  })
 
-  Router.route('/', function() {
-    this.render('home')
-    AceEditor.instance('xmlbox', {
-      theme: 'dawn',
-      mode: 'xml'
-    }, function(editor) {
-      editor.setReadOnly(true)
-      editor.setOptions({
-        maxLines: Infinity,
-        showLineNumbers: false
-      })
-      editor.on('change', function() {
-        Session.set('xml', editor.getValue())
-      })
-    })
-  })
+    function feedback(message, success) {
+        var $feedback = document.getElementById('feedback');
+        $feedback.setAttribute('class', success? 'teal lighten-3 white-text' : 'red lighten-3');
+        $feedback.innerText = message;
+        setTimeout(function() {
+            $feedback.setAttribute('class', '');
+            $feedback.innerText = '';
+        }, 3000);
+    }
 
-  function updateRegistered(inputs, registeredPiis) {
-    Session.set('registeredPiis', registeredPiis.filter(function(registered) {
-      return inputs.indexOf(registered.pii) !== -1
-    }))
-  }
+    function clearEditor() {
+        AceEditor.instance('xmlbox', null, function(editor) {
+            editor.setValue('');
+            editor.clearSelection();
+            editor.setReadOnly(true);
+        });
+    }
 
-  function feedback(message, success) {
-    var $feedback = document.getElementById('feedback')
-    $feedback.setAttribute('class', success? 'teal lighten-3 white-text' : 'red lighten-3')
-    $feedback.innerText = message
-    setTimeout(function() {
-      $feedback.setAttribute('class', '')
-      $feedback.innerText = ''
-    }, 3000)
-  }
-
-  function clearEditor() {
-    AceEditor.instance('xmlbox', null, function(editor) {
-      editor.setValue('')
-      editor.clearSelection()
-      editor.setReadOnly(true)
-    })
-  }
-
-  function loadEditor(xml) {
-    AceEditor.instance('xmlbox', null, function(editor) {
-      editor.setValue(xml)
-      editor.clearSelection()
-      editor.setReadOnly(false)
-      editor.focus()
-    })
-  }
+    function loadEditor(xml) {
+        AceEditor.instance('xmlbox', null, function(editor) {
+            editor.setValue(xml);
+            editor.clearSelection();
+            editor.setReadOnly(false);
+            editor.focus();
+        });
+    }
 }
