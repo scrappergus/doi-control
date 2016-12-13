6cfaa783afa6fd06455468eefe26ac87cd7c594d
Gus Clemens <scrappergus@gmail.com>
2016-12-13 11:12:45 -0700

    This is unfinished code, using the alternate http method for submitting metadata XML to CrossRef.

--- server/method_submitXML.js
+++ server/method_submitXML.js
@@ -5,20 +5,51 @@ Meteor.methods({
     return Meteor.call('submit_xml', xml, test);
   },
 
+//  submit_xml: function(xml, test) {
+//    var baseUrl = 'https://api.crossref.org';
+//    var options = {
+//      headers: {
+//        'Authorization': 'Basic ' + Base64.encode(Meteor.settings.cr_cred_un + ':' + Meteor.settings.cr_cred_pw),
+//        'Content-Type': 'application/vnd.crossref.deposit+xml'
+//      },
+//      content: xml
+//    };
+//    var encoded_pingback = encodeURIComponent('doi.oncotarget.com/deposit_pingback');
+//    var path = baseUrl + '/deposits?' + (test ? 'test=true&' : '') + 'pingback=' + encoded_pingback;
+//    try {
+//        console.log(path);
+//        console.log(options);
+//      response = Meteor.http.call('POST', path, options);
+//    } catch (err) {
+//        console.error(err);
+//      return {
+//        error: baseUrl + ' says ' + err.message
+//      };
+//    }
+//    return JSON.stringify(response);
+////    return {
+////      code: response && response.statusCode,
+////      location: response && response.headers && response.headers.location || ''
+////    }
+//  }
   submit_xml: function(xml, test) {
-    var baseUrl = 'https://api.crossref.org';
+    var baseUrl = 'https://doi.crossref.org/servlet/deposit/';
     var options = {
-      headers: {
-        'Authorization': 'Basic ' + Base64.encode(Meteor.settings.cr_cred_un + ':' + Meteor.settings.cr_cred_pw),
-        'Content-Type': 'application/vnd.crossref.deposit+xml'
-      },
-      content: xml
+        'params':{
+            'operation':'doMDUpload',
+            'login_id':'login_id',
+            'login_passwd':'password'
+        },
+        content: xml
     };
     var encoded_pingback = encodeURIComponent('doi.oncotarget.com/deposit_pingback');
-    var path = baseUrl + '/deposits?' + (test ? 'test=true&' : '') + 'pingback=' + encoded_pingback;
+    var path = baseUrl;// + '/deposits?' + (test ? 'test=true&' : '') + 'pingback=' + encoded_pingback;
     try {
+        console.log(path);
+        console.log(options);
       response = Meteor.http.call('POST', path, options);
     } catch (err) {
+        console.error(err);
       return {
         error: baseUrl + ' says ' + err.message
       };
@@ -29,4 +60,5 @@ Meteor.methods({
 //      location: response && response.headers && response.headers.location || ''
 //    }
   }
+
 })
